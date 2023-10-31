ab7f505d459c07e8fa313d9f68c2a6b507ae7c5b
Jacob Kopacz <jacobkopacz@235.187.20.172.s.wireless.uri.edu>
2023-10-31 13:41:45 -0400

    forgot to add Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Connection conn = DatabaseConnection.getConnection("Inventory");
+
+        DatabaseConnection.closeConnection(conn);
+        System.out.println("HI");
+    }
 }
