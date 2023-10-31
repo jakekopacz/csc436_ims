15091e6ffa61c2253b79014960cfc580ebe68b8a
Jake Kopacz <jacob_kopacz@uri.edu>
2023-10-31 13:43:19 -0400

    Update database.sql

--- sql_files/database.sql
+++ sql_files/database.sql
@@ -1,6 +1,7 @@
 create database Inventory;
-Use Inventory;
+use Inventory;
 
+###tables that hold main entity data
 
 create table Customer (
     email varchar(32),
@@ -23,19 +24,16 @@ create table Item (
     primary key(item_id)
 );
 
-#Many to many relationship between supplier and item.(i.e 1 item is supplied by 3 suppliers, 3 items are supplied by one supplier)
-#cost is in this table bc different suppliers may have different cost.
-Create table SupplierList (
-	cost INT,
-	supplier_email varchar(32) references Supplier(email),
-    item_id INT references Item(item_id),
-    primary key(supplier_email, item_id)
-);
+###tables that relate Item to the other main entities, essentially holding order data
+#both incoming/outgoing orders are split into two tables, since items are multivalued attributes: 
+#	for customers and item, we have SalesOrder and ItemOrder
+#	for suppliers and item, we have ReplenishOrder and ItemReplenish
+#suppliers and items are many to many, so this data is stored in SupplierList
 
-#Total attribute is redundent
+
+#table holds data on customer sales
 create table SalesOrder (
     order_id int,
-    total numeric(8,2),
     shipping_option varchar(16),
     tracking_num varchar(32),
     customer_email varchar(32),
@@ -43,10 +41,9 @@ create table SalesOrder (
     foreign key(customer_email) references Customer(email)
 );
 
-#Total attribute is redundent
+#table holds data on supplier orders
 create table ReplenishOrder (
 	replenish_id int,
-    total numeric(8,2),
 	shipping_option varchar(16),
     tracking_num varchar(32),
     supplier_email varchar(32),
@@ -54,8 +51,9 @@ create table ReplenishOrder (
     foreign key(supplier_email) references Supplier(email)
 );
 
-#one order to many itemOrder
-#one item to many itemOrder
+
+#table relates customer order to items ordered and their quantities
+#one order can have many items, and one item can have many orders
 create table ItemOrder (
     order_id int,
     item_id int,
@@ -65,7 +63,7 @@ create table ItemOrder (
     foreign key(order_id) references SalesOrder(order_id)
 );
 
-#same as above, this and ItemReplenish could be combined but I think it might complicate things
+#table relates supplier order to items ordered and their quantities; similar many to many relation as above
 create table ItemReplenish (
     replenish_id int,
     item_id int,
@@ -75,6 +73,18 @@ create table ItemReplenish (
     foreign key(replenish_id) references ReplenishOrder(replenish_id)
 );
 
+#table relates suppliers to their respective items
+#Many to many relationship between supplier and item.(i.e 1 item is supplied by 3 suppliers, 3 items are supplied by one supplier). Primary key is each supplier/item tuple.
+#cost is in this table bc different suppliers may have different cost.
+Create table SupplierList (
+        cost INT,
+        supplier_email varchar(32) references Supplier(email),
+    item_id INT references Item(item_id),
+    primary key(supplier_email, item_id)
+);
+
+
+###tables hold contact info for suppliers/customers and check for one unique email address
 create table MailingList(
     customer_email varchar(32),
     supplier_email varchar(32),
@@ -85,10 +95,10 @@ create table MailingList(
 	state char(2), 
     country char(2),
     postal_code varchar(16),
-    primary key(customer_email,supplier_email),
+    unique(customer_email,supplier_email),
     foreign key(customer_email) references Customer(email),
     foreign key(supplier_email) references Supplier(email),
-    check(customer_email is null or supplier_email is null and customer_email != supplier_email)
+    check(customer_email is null and supplier_email is not null or customer_email is not null and supplier_email is null)
 );
 
 create table PhoneList(
@@ -97,8 +107,8 @@ create table PhoneList(
     work_phone varchar(16),
     home_phone varchar(16),
     cell_phone varchar(16),
-    primary key(customer_email,supplier_email),
+    unique(customer_email,supplier_email),
     foreign key(customer_email) references Customer(email),
     foreign key(supplier_email) references Supplier(email),
-    check(customer_email is null or supplier_email is null and customer_email != supplier_email)
+    check(customer_email is null and supplier_email is not null or customer_email is not null and supplier_email is null)
 );
