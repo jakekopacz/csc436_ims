525a35f8df54334ea326f34436d70d426c554566
Jake Kopacz <jacob_kopacz@uri.edu>
2023-10-31 13:43:40 -0400

    Update queriesAndMore.sql

--- sql_files/queriesAndMore.sql
+++ sql_files/queriesAndMore.sql
@@ -1,64 +1,75 @@
-Use Inventory;
+use Inventory;
+###BASIC QUERIES
 
 #QUERY, INSERT, DELETE, UPDATE INTO CUSTOMER
 INSERT INTO Customer(email, credit_card)
 VALUES('customerDEMO@gmail.com','fakefake');
+UPDATE Customer SET credit_card = 'updatedfake' WHERE email='customerDEMO@gmail.com';
+DELETE FROM Customer WHERE email = 'customerDEMO@gmail.com';
 
 #QUERY, INSERT, DELETE, UPDATE INTO SUPPLIER
 INSERT INTO Supplier(email, account_balance, lead_time)
 VALUES('supplierDEMO@gmail.com', 0, 6);
+UPDATE Supplier SET account_balance = 1 WHERE email='supplierDEMO@gmail.com';
+DELETE FROM Customer WHERE email='supplierDEMO@gmail.com';
 
 #QUERY, INSERT, DELETE, UPDATE INTO ITEM
 INSERT INTO Item(item_id, quantity, price, category)
 Values(99, 999, 999, 'demo');
+UPDATE Item SET price = 1000 WHERE item_id = 99;
+DELETE FROM Item WHERE item_id = 99;
+
+#QUERY, INSERT, DELETE, UPDATE INTO SALESORDER
+INSERT INTO SalesOrder(order_id, shipping_option, tracking_num, customer_email)
+#customer_email is a FK constraint here
+VALUES (4, 'express', '123ship', 'cust2@gmail.com');
+UPDATE SalesOrder SET shipping_option = 'regular' WHERE order_id = 4;
+DELETE FROM SalesOrder WHERE order_id = 4;
+
+#QUERY, INSERT, DELETE, UPDATE INTO REPLENISHORDER
+INSERT INTO ReplenishOrder(replenish_id, shipping_option, tracking_num, supplier_email)
+VALUES (4, 'standard', '321ship', 'supplierDEMO@gmail.com');
+UPDATE ReplenishOrder SET tracking_num = 'ship321' WHERE replenish_id = 4;
+DELETE FROM ReplenishOrder WHERE replenish_id = 4;
+
+#QUERY, INSERT, DELETE, UPDATE INTO ITEMORDER
+INSERT INTO ItemOrder(order_id, item_id, quantity)
+VALUES(3, 1, 20);
+#cancel item from order
+UPDATE ItemOrder SET quantity = 0 WHERE order_id = 3 and ItemOrder.item_id = 1;
+#cancel entire order without deleting
+UPDATE ItemOrder SET quantity = 0 WHERE order_id = 3;
+DELETE FROM ItemOrder WHERE order_id = 3;
+
+#QUERY, INSERT, DELETE, UPDATE INTO ITEMREPLENISH
+INSERT INTO ItemReplenish(replenish_id, item_id, quantity)
+VALUES(1, 3, 20);
+UPDATE ItemReplenish SET quantity = 30 WHERE replenish_id = 1 and ItemReplenish.item_id = 3;
+DELETE FROM ItemReplenish WHERE replenish_id = 1;
 
 #QUERY, INSERT, DELETE, UPDATE INTO SUPPLIERLIST
 INSERT INTO SupplierList(supplier_email, item_id)
 VALUES ('supplierDEMO@gmail.com',99);
-
-#QUERY, INSERT, DELETE, UPDATE INTO SALESORDER
-INSERT INTO SalesOrder(order_id, total, shipping_option, tracking_num, customer_email)
-VALUES (1, 123, 'express', '123ship', 'customerDEMO@gmail.com');
-
-#QUERY, INSERT, DELETE, UPDATE INTO REPLENISHORDER
-INSERT INTO ReplenishOrder(replenish_id, total, shipping_option, tracking_num, customer_email)
-VALUES (1, 123, 'standard', '321ship', 'supplierDEMO@gmail.com');
-
-#QUERY, INSERT, DELETE, UPDATE INTO ITEMORDER
-INSERT INTO ItemOrder(order_id, item_id, quantity)
-VALUES(1, 1, 10);
+UPDATE SupplierList SET item_id = 100 WHERE supplier_email = 'supplierDEMO@gmail.com';
+DELETE FROM SupplierList WHERE supplier_email = 'supplierDEMO@gmail.com';
 
 
-#SPECIAL QUARIES
+###SPECIAL QUERIES
 
-create view order_item_quantity_price(order_id, item_id, quantity, price, total_price) as
-	SELECT ItemOrder.*, Item.price, (ItemOrder.quantity * Item.Price)
-    FROM ItemOrder LEFT OUTER JOIN Item ON ItemOrder.item_id = Item.item_id;
-    
-#View the Salesorders and the total $Price
-create view SalesOrder_total_price as
-	SELECT order_id, sum(total_price)
-    FROM order_item_quantity_price
-    GROUP BY order_id;
-    
-#View the ReplenishOrders and the total $Cost
-#Since cost can differ from supplier it is necissary to create a view that has supplier_email and replenish_id in it.
-create view ItemReplenish_supplier_email as 
-	SELECT ItemReplenish.*, ReplenishOrder.supplier_email
-    FROM ItemReplenish LEFT OUTER JOIN ReplenishOrder ON ItemReplenish.replenish_id = ReplenishOrder.replenish_id;
-    
-create view ReplenishOrder_total as
-	SELECT ItemReplenish_supplier_email.replenish_id, sum(SupplierList.cost * ItemReplenish_supplier_email.quantity)
-    FROM ItemReplenish_supplier_email LEFT OUTER JOIN SupplierList ON (ItemReplenish_supplier_email.supplier_email = SupplierList.supplier_email) AND (ItemReplenish_supplier_email.item_id = SupplierList.item_id)
-    GROUP BY ItemReplenish_supplier_email.replenish_id;
+#show all tables
+show tables;
 
-#View suppliers and the number items they supply, grouped by supplier
-create view supplier_number_of_items(supplier_email, items_supplied) as
-	SELECT supplier_email, count(item_id) FROM supplierList
-	group by supplier_email;
-    
-#View items and the number of suppliers they have, grouped by item
-create view item_number_of_suppliers(item_id, suppliers) as
-	SELECT item_id, count(supplier_email) FROM supplierList
-	group by item_id;
+#altering a table
+ALTER TABLE SalesOrder ADD shipping_fee numeric(5,2); 
+ALTER TABLE ItemOrder RENAME column quantity to item_quantity;
+#revert
+ALTER TABLE ItemOrder RENAME column item_quantity to quantity;
+ALTER TABLE SalesOrder DROP column shipping_fee;
 
+#show items out of stock
+INSERT INTO Item(item_id, quantity, price, category)
+Values(99, 0, 999, 'demo');
+SELECT item_id from Item where quantity = 0;
+
+#show how many days it takes for a restocking to work through the supply chain
+select item_id, sum(lead_time) from Supplier natural join SupplierList group by item_id;
