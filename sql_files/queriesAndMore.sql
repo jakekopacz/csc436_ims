600bb4fd40e92afe8e744391153b2e3db5ee44ad
Jacob Kopacz <jacobkopacz@Jacobs-MacBook-Pro-3.local>
2023-11-22 13:20:41 -0500

    t

--- sql_files/queriesAndMore.sql
+++ sql_files/queriesAndMore.sql
@@ -1,64 +1,114 @@
-Use Inventory;
+create database Inventory;
+use Inventory;
 
-#QUERY, INSERT, DELETE, UPDATE INTO CUSTOMER
-INSERT INTO Customer(email, credit_card)
-VALUES('customerDEMO@gmail.com','fakefake');
+###tables that hold main entity data
 
-#QUERY, INSERT, DELETE, UPDATE INTO SUPPLIER
-INSERT INTO Supplier(email, account_balance, lead_time)
-VALUES('supplierDEMO@gmail.com', 0, 6);
+create table Customer (
+                          email varchar(32),
+                          credit_card varchar(16),
+                          primary key(email)
+);
 
-#QUERY, INSERT, DELETE, UPDATE INTO ITEM
-INSERT INTO Item(item_id, quantity, price, category)
-Values(99, 999, 999, 'demo');
+create table Supplier (
+                          email varchar(32),
+                          account_balance numeric(8,2),
+                          lead_time int,
+                          primary key(email)
+);
 
-#QUERY, INSERT, DELETE, UPDATE INTO SUPPLIERLIST
-INSERT INTO SupplierList(supplier_email, item_id)
-VALUES ('supplierDEMO@gmail.com',99);
+create table Item (
+                      item_id int,
+                      quantity int,
+                      price numeric(7,2),
+                      category varchar(32),
+                      primary key(item_id)
+);
 
-#QUERY, INSERT, DELETE, UPDATE INTO SALESORDER
-INSERT INTO SalesOrder(order_id, total, shipping_option, tracking_num, customer_email)
-VALUES (1, 123, 'express', '123ship', 'customerDEMO@gmail.com');
-
-#QUERY, INSERT, DELETE, UPDATE INTO REPLENISHORDER
-INSERT INTO ReplenishOrder(replenish_id, total, shipping_option, tracking_num, customer_email)
-VALUES (1, 123, 'standard', '321ship', 'supplierDEMO@gmail.com');
-
-#QUERY, INSERT, DELETE, UPDATE INTO ITEMORDER
-INSERT INTO ItemOrder(order_id, item_id, quantity)
-VALUES(1, 1, 10);
+###tables that relate Item to the other main entities, essentially holding order data
+#both incoming/outgoing orders are split into two tables, since items are multivalued attributes:
+#	for customers and item, we have SalesOrder and ItemOrder
+#	for suppliers and item, we have ReplenishOrder and ItemReplenish
+#suppliers and items are many to many, so this data is stored in SupplierList
 
 
-#SPECIAL QUARIES
+#table holds data on customer sales
+create table SalesOrder (
+                            order_id int,
+                            shipping_option varchar(16),
+                            tracking_num varchar(32),
+                            customer_email varchar(32),
+                            primary key(order_id),
+                            foreign key(customer_email) references Customer(email)
+);
 
-create view order_item_quantity_price(order_id, item_id, quantity, price, total_price) as
-	SELECT ItemOrder.*, Item.price, (ItemOrder.quantity * Item.Price)
-    FROM ItemOrder LEFT OUTER JOIN Item ON ItemOrder.item_id = Item.item_id;
-    
-#View the Salesorders and the total $Price
-create view SalesOrder_total_price as
-	SELECT order_id, sum(total_price)
-    FROM order_item_quantity_price
-    GROUP BY order_id;
-    
-#View the ReplenishOrders and the total $Cost
-#Since cost can differ from supplier it is necissary to create a view that has supplier_email and replenish_id in it.
-create view ItemReplenish_supplier_email as 
-	SELECT ItemReplenish.*, ReplenishOrder.supplier_email
-    FROM ItemReplenish LEFT OUTER JOIN ReplenishOrder ON ItemReplenish.replenish_id = ReplenishOrder.replenish_id;
-    
-create view ReplenishOrder_total as
-	SELECT ItemReplenish_supplier_email.replenish_id, sum(SupplierList.cost * ItemReplenish_supplier_email.quantity)
-    FROM ItemReplenish_supplier_email LEFT OUTER JOIN SupplierList ON (ItemReplenish_supplier_email.supplier_email = SupplierList.supplier_email) AND (ItemReplenish_supplier_email.item_id = SupplierList.item_id)
-    GROUP BY ItemReplenish_supplier_email.replenish_id;
+#table holds data on supplier orders
+create table ReplenishOrder (
+                                replenish_id int,
+                                shipping_option varchar(16),
+                                tracking_num varchar(32),
+                                supplier_email varchar(32),
+                                primary key(replenish_id),
+                                foreign key(supplier_email) references Supplier(email)
+);
 
-#View suppliers and the number items they supply, grouped by supplier
-create view supplier_number_of_items(supplier_email, items_supplied) as
-	SELECT supplier_email, count(item_id) FROM supplierList
-	group by supplier_email;
-    
-#View items and the number of suppliers they have, grouped by item
-create view item_number_of_suppliers(item_id, suppliers) as
-	SELECT item_id, count(supplier_email) FROM supplierList
-	group by item_id;
 
+#table relates customer order to items ordered and their quantities
+#one order can have many items, and one item can have many orders
+create table ItemOrder (
+                           order_id int,
+                           item_id int,
+                           quantity int,
+                           primary key(order_id, item_id),
+                           foreign key(item_id) references Item(item_id),
+                           foreign key(order_id) references SalesOrder(order_id)
+);
+
+#table relates supplier order to items ordered and their quantities; similar many to many relation as above
+create table ItemReplenish (
+                               replenish_id int,
+                               item_id int,
+                               quantity int,
+                               primary key(replenish_id, item_id),
+                               foreign key(item_id) references Item(item_id),
+                               foreign key(replenish_id) references ReplenishOrder(replenish_id)
+);
+
+#table relates suppliers to their respective items
+#Many to many relationship between supplier and item.(i.e 1 item is supplied by 3 suppliers, 3 items are supplied by one supplier). Primary key is each supplier/item tuple.
+#cost is in this table bc different suppliers may have different cost.
+Create table SupplierList (
+        cost INT,
+        supplier_email varchar(32) references Supplier(email),
+    item_id INT references Item(item_id),
+    primary key(supplier_email, item_id)
+);
+
+
+###tables hold contact info for suppliers/customers and check for one unique email address
+create table MailingList(
+                            customer_email varchar(32),
+                            supplier_email varchar(32),
+                            address_1 varchar(128),
+                            address_2 varchar(128),
+                            address_3 varchar(128),
+                            city varchar(128),
+                            state char(2),
+                            country char(2),
+                            postal_code varchar(16),
+                            unique(customer_email,supplier_email),
+                            foreign key(customer_email) references Customer(email),
+                            foreign key(supplier_email) references Supplier(email),
+                            check(customer_email is null and supplier_email is not null or customer_email is not null and supplier_email is null)
+);
+
+create table PhoneList(
+                          customer_email varchar(32),
+                          supplier_email varchar(32),
+                          work_phone varchar(16),
+                          home_phone varchar(16),
+                          cell_phone varchar(16),
+                          unique(customer_email,supplier_email),
+                          foreign key(customer_email) references Customer(email),
+                          foreign key(supplier_email) references Supplier(email),
+                          check(customer_email is null and supplier_email is not null or customer_email is not null and supplier_email is null)
+);
